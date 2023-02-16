f38eab65bc210b03d9fb60cb7a35b575df5c4172
Shivam Jha <71783722+shivam-jha2712@users.noreply.github.com>
2023-02-16 18:13:59 +0530

    commit

---
+++ Class_Codes/printing.py
@@ -0,0 +1,9 @@
+print("Hello World")
+print(1 + 2)
+print(10 * 3.5)
+print()
+print("I am the best")
+print("I", "am", "the", "best", sep="__", end="@@")
+# Here print statement can take 5 different params and amongst them
+# sep is used to determine the seperator in python
+# end is used to determine the ending statement in python
