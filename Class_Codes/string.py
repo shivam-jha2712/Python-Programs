9273f93f3a911860b7ea1658f02458e7e42d43f6
Shivam Jha <71783722+shivam-jha2712@users.noreply.github.com>
2023-02-16 18:31:01 +0530

    commit

---
+++ Class_Codes/string.py
@@ -0,0 +1,11 @@
+print('I am "Loving" it')
+print("I am 'Loving' it")
+
+# How to concatenate and take input
+# It is important to note that input in python by default is of type string
+variable = "Your name is:"
+name = input("Enter your name: ")
+
+print(variable + " " + name)
+# Space is provided in the output of the above code and no space is there in the output of the below code
+print(variable + name)
