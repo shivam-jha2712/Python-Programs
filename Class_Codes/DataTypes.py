9cc042ed579e916b421c4431d57bb3d9da8346ea
Shivam Jha <71783722+shivam-jha2712@users.noreply.github.com>
2023-02-19 13:50:59 +0530

    commit

---
+++ Class_Codes/DataTypes.py
@@ -0,0 +1,19 @@
+# Numeric DataTypes : 1.int, 2.float, 3.complex
+# 1. int:
+#       a. it has no upper or lower limit in terms of size in python only depends upon computer memory
+#       b. it can contain any number that is not decimal (i.e; Decimals, Binary, Octal, HexaDecimal)
+# Examples in code:
+print(type(10))
+print(type(0b10)) # this is binary (binary prefix is 0b or 0B)
+print(type(0o16)) # this is octal (octal prefix is 0o or 0O)
+print(type(0x69)) # this is hexadecimal (hexadecimal prefix is 0x or 0X)
+
+# 2. float:
+#           a. it contains all the decimal values and any kind of decimal values
+#           b. similar to int in float as well it does not contains any upperlimit for the size to be stored in python
+
+# Examples in code:
+print(type(10.75))
+
+
+
---
+++ Class_Codes/variables.py
@@ -0,0 +1,24 @@
+# string1 = "Jai Shri Ram"
+# print(type(string1))
+# print(type("Jai Shri Ram"))
+# print(type(10))
+# print(type(10.7))
+
+# Below is assigning data to variables
+#
+x = "Shivam"
+y = "is"
+z = "Ram Bhakt"
+#
+print(x + " " + y + " " + z)
+
+# Else you can even do the same as above but using comma operator and thus having no issue of providing seperators
+print(x, y, z)
+
+# Why python is said as a strongly typed language
+# x = 27
+# y = "Shivam"
+
+# inbuilt functions like isinstance() and issubclass() are used to test the type of the variable automatically and
+# assign its correct usage
+# print(x + y)
